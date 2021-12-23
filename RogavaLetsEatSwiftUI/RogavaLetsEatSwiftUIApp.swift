a747f529154195a086aea8796f4c545cb09fb177
giorgirogava <giorgirogava01ai@gmail.com>
2021-12-23 19:42:00 +0400

    Initial Commit

---
+++ RogavaLetsEatSwiftUI/RogavaLetsEatSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RogavaLetsEatSwiftUIApp.swift
+//  RogavaLetsEatSwiftUI
+//
+//  Created by IMAC on 23.12.21.
+//
+
+import SwiftUI
+
+@main
+struct RogavaLetsEatSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
